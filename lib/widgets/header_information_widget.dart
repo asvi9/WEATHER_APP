16555223158667f58db84949dc0345971de146f9
Ashwani <asvi2232@gmail.com>
2025-04-01 22:35:02 +0530

    First Commit
    upload  code

---
+++ lib/widgets/header_information_widget.dart
@@ -0,0 +1,56 @@
+
+import 'package:day_night_switcher/day_night_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/home_controller.dart';
+import '../utils/date_time_format.dart';
+
+class HeaderInformationWidget extends StatelessWidget {
+  const HeaderInformationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.find<HomeController>();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Obx(
+                    () => Text(
+                      controller.city.value,
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(
+                    Icons.location_on,
+                    size: 25,
+                  ),
+                ],
+              ),
+              Text(
+                DateTimeFormat.currentDate,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
+          DayNightSwitcher(
+            isDarkModeEnabled: controller.isDarkMode.value,
+            onStateChanged: (value) {
+              controller.toggleTheme();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/hourly_weather_data_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/home_controller.dart';
+import '../model/weather_data_hourly.dart';
+import 'hourly_detail_container_widget.dart';
+
+
+class HourlyWeatherDataWidget extends StatelessWidget {
+  const HourlyWeatherDataWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HomeController controller = Get.find<HomeController>();
+    final WeatherDataHourly? weatherDataHourly =
+        controller.weatherData.value?.hourly;
+
+    return Column(
+      children: [
+        Text("Today", style: Theme.of(context).textTheme.titleLarge),
+        const SizedBox(height: 20),
+        SizedBox(
+          height: 160,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: (weatherDataHourly?.hourly != null &&
+                    weatherDataHourly!.hourly.length > 12)
+                ? 12
+                : weatherDataHourly?.hourly.length ?? 0,
+            itemBuilder: (context, index) {
+              return HourlyDetailContainerWidget(
+                temp: weatherDataHourly?.hourly[index].temp ?? 0,
+                timeStamp: weatherDataHourly?.hourly[index].dt ?? 0,
+                weatherIcon:
+                    weatherDataHourly?.hourly[index].weather![0].icon ?? '',
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
